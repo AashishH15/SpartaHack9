16afda17238fd805a1372a8245daeab529cccef6
Aashish Harishchandre <aashish0415@gmail.com>
2024-01-27 18:07:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,13 +11,14 @@ gdf = gdf[gdf['name'].isin(countries_to_mark)]
 app = Flask(__name__)
 
 def generate_map():
-    m = folium.Map(location=[0, 0], zoom_start=2, tiles='OpenStreetMap', min_zoom=2)
+    m = folium.Map(location=[0, 0], zoom_start=2, min_zoom=2)
+    folium.TileLayer('cartodbdark_matter').add_to(m)
 
     for _, r in gdf.iterrows():
         sim_geo = gpd.GeoSeries(r['geometry']).simplify(tolerance=0.001)
         geo_j = sim_geo.to_json()
         geo_j = folium.GeoJson(data=geo_j,
-                               style_function=lambda x: {'fillColor': 'orange'})
+                               style_function=lambda x: {'fillColor': 'yellow'})
 
         folium.Marker(
             location=[r['geometry'].centroid.y, r['geometry'].centroid.x],
