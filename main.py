a69e7750ea7020c138e657f75622d5b47421ed3e
Aashish Harishchandre <aashish0415@gmail.com>
2024-01-27 16:30:38 -0500

    Front end iFrame

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template_string
+
+from flask import Flask, render_template
 import geopandas as gpd
 import folium
 
@@ -6,27 +7,33 @@ gdf = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
 
 app = Flask(__name__)
 
-@app.route('/')
-def map():
+def generate_map():
     m = folium.Map(location=[0, 0], zoom_start=2, tiles='CartoDB positron')
-    
+
     for _, r in gdf.iterrows():
         sim_geo = gpd.GeoSeries(r['geometry']).simplify(tolerance=0.001)
         geo_j = sim_geo.to_json()
         geo_j = folium.GeoJson(data=geo_j,
                                style_function=lambda x: {'fillColor': 'orange'})
-        
+
         folium.Marker(
             location=[r['geometry'].centroid.y, r['geometry'].centroid.x],
             popup=folium.Popup(r['name']),
             icon=folium.Icon(color="red", icon="info-sign")
         ).add_to(m)
-        
+
         geo_j.add_to(m)
-    
-    
+
     return m._repr_html_()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/map-content')
+def map_content():
+    map_html = generate_map()
+    return map_html
 
 if __name__ == '__main__':
     app.run(debug=True)
-
