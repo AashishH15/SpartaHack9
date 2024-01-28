772a58f095c168f9be6c8a9dcf730bba77212650
Skizmo42 <eman06040@icloud.com>
2024-01-27 22:42:10 -0500

    Updated bar graph

--- bargraph.py
+++ bargraph.py
@@ -3,12 +3,33 @@ import plotly.express as px
 
 def create_bar_chart():    
     # Read data from CSV file
-    file_path = 'info.csv'
+    file_path = 'BKW_data.csv'
     data = pd.read_csv(file_path)
 
     # Create a bar graph using plotly
-    fig = px.bar(data, x='Country', y='Population (millions)', color='Currency',
-                labels={'Population (millions)': 'Population (Millions)'}, title='Population of Countries')
+    fig = px.bar(data, x='name', y='dollar_price', color_discrete_sequence=['yellow'],
+                labels={'Country': 'Cost of Whopper (USD)'}, title='The Whopper Index')
+
+    fig.update_layout(
+    plot_bgcolor='#262626',
+    paper_bgcolor='#262626',
+    font_color='white',
+    font_family='Arial',
+    font_size=18,
+    title_font_family='Arial',
+    title_font_color='white',
+    title_font_size=24,
+    legend_title_font_color='white',
+    legend_title_font_size=20,
+    legend_font_family='Arial',
+    legend_font_color='white',
+    legend_font_size=18,
+    xaxis_title_font_family='Arial',
+    xaxis_title_font_color='white',
+    xaxis_title_font_size=20,
+    yaxis_title_font_family='Arial',
+    yaxis_title_font_color='white',
+    )
 
     # Convert the plot to HTML
     graph_html = fig.to_html()
