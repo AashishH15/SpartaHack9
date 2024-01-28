6440b3f81e2bf168f54c2a087069f2377e1b0764
Aashish Harishchandre <aashish0415@gmail.com>
2024-01-27 20:15:06 -0500

    front end update and file moved

---
+++ map.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template
+import geopandas as gpd
+import pandas as pd
+import folium
+
+gdf = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+
+countries_to_mark = ['United States of America', 'China', 'Germany', 'India', 'Japan']
+
+gdf = gdf[gdf['name'].isin(countries_to_mark)]
+
+app = Flask(__name__)
+
+def generate_map():
+    m = folium.Map(location=[0, 0], zoom_start=1, min_zoom=2, tiles='cartodbdark_matter')
+
+    for _, r in gdf.iterrows():
+        sim_geo = gpd.GeoSeries(r['geometry']).simplify(tolerance=0.001)
+        geo_j = sim_geo.to_json()
+        geo_j = folium.GeoJson(data=geo_j,
+                               style_function=lambda x: {'fillColor': 'yellow', 'color': 'yellow'})
+
+        folium.Marker(
+            location=[r['geometry'].centroid.y, r['geometry'].centroid.x],
+            popup=folium.Popup(r['name']),
+            icon=folium.Icon(color="black", icon="cutlery")
+        ).add_to(m)
+
+        geo_j.add_to(m)
+
+    return m._repr_html_()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/map-content')
+def map_content():
+    map_html = generate_map()
+    return map_html
+
+if __name__ == '__main__':
+    app.run(debug=True)
