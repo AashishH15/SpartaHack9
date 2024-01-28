e6ca935d545e9475ecc73988c4a4b8a12af1a3d4
ethancook2 <ethancook322@gmail.com>
2024-01-28 11:28:52 -0500

    fixed starbucks and mcd csvs

--- bargraph.py
+++ bargraph.py
@@ -5,7 +5,7 @@ def create_bar_chart(filename):
     data = pd.read_csv(filename)
 
     fig = px.bar(data, x='name', y='dollar_price', color='dollar_price', color_continuous_scale='YlOrRd',
-                labels={'Country': 'Cost of Whopper (USD)'}, title='The Whopper Index')
+                labels={'Country': 'Cost of Whopper (USD)'})
 
     fig.update_traces(marker_line_width=1.5, marker_line_color='rgb(8,48,107)')
 
