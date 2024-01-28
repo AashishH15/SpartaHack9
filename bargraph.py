6e697274e81ae6dc016aff94d62c6ec789a9613b
Aashish Harishchandre <aashish0415@gmail.com>
2024-01-28 03:51:58 -0500

    Update bargraph.py

--- bargraph.py
+++ bargraph.py
@@ -2,7 +2,6 @@ import pandas as pd
 import plotly.express as px
 
 def create_bar_chart():    
-    # Read data from CSV file
     file_path = 'BKW_data.csv'
     data = pd.read_csv(file_path)
 
@@ -15,20 +14,20 @@ def create_bar_chart():
         plot_bgcolor= '#262626',
         paper_bgcolor='#262626',
         font_color='White',
-        font_family='Arial',
+        font_family='Helvetica',
         font_size=18,
-        title_font_family='Arial',
-        title_font_color='black',
+        title_font_family='Helvetica',
+        title_font_color='White',
         title_font_size=24,
         legend_title_font_color='White',
         legend_title_font_size=20,
-        legend_font_family='Arial',
+        legend_font_family='Helvetica',
         legend_font_color='White',
         legend_font_size=18,
-        xaxis_title_font_family='Arial',
+        xaxis_title_font_family='Helvetica',
         xaxis_title_font_color='White',
         xaxis_title_font_size=20,
-        yaxis_title_font_family='Arial',
+        yaxis_title_font_family='Helvetica',
         yaxis_title_font_color='White',
         yaxis_title_font_size=20,
     )
