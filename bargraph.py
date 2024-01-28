b2897a1a06d225d448a104a6208b90d47c688a57
Aashish Harishchandre <aashish0415@gmail.com>
2024-01-28 03:49:00 -0500

    Update bargraph.py

--- bargraph.py
+++ bargraph.py
@@ -7,28 +7,30 @@ def create_bar_chart():
     data = pd.read_csv(file_path)
 
     # Create a bar graph using plotly
-    fig = px.bar(data, x='name', y='dollar_price', color_discrete_sequence=['yellow'],
-                labels={'Country': 'Cost of Whopper (USD)'}, title='The Whopper Index')
+    fig = px.bar(data, x='name', y='dollar_price', color='dollar_price', color_continuous_scale='Bluered_r',
+                labels={'name': 'Country', 'dollar_price': 'Cost of Whopper (USD)'}, 
+                title='The Whopper Index', hover_data=['name', 'dollar_price'])
 
     fig.update_layout(
-    plot_bgcolor='#262626',
-    paper_bgcolor='#262626',
-    font_color='white',
-    font_family='Arial',
-    font_size=18,
-    title_font_family='Arial',
-    title_font_color='white',
-    title_font_size=24,
-    legend_title_font_color='white',
-    legend_title_font_size=20,
-    legend_font_family='Arial',
-    legend_font_color='white',
-    legend_font_size=18,
-    xaxis_title_font_family='Arial',
-    xaxis_title_font_color='white',
-    xaxis_title_font_size=20,
-    yaxis_title_font_family='Arial',
-    yaxis_title_font_color='white',
+        plot_bgcolor= '#262626',
+        paper_bgcolor='#262626',
+        font_color='White',
+        font_family='Arial',
+        font_size=18,
+        title_font_family='Arial',
+        title_font_color='black',
+        title_font_size=24,
+        legend_title_font_color='White',
+        legend_title_font_size=20,
+        legend_font_family='Arial',
+        legend_font_color='White',
+        legend_font_size=18,
+        xaxis_title_font_family='Arial',
+        xaxis_title_font_color='White',
+        xaxis_title_font_size=20,
+        yaxis_title_font_family='Arial',
+        yaxis_title_font_color='White',
+        yaxis_title_font_size=20,
     )
 
     graph_html = fig.to_html()
